862e95edee939927f3c4fa54d99306d37747dc4e
Thiago Araujo <thiago.tg.k2@hotmail.com>
2021-05-17 11:52:29 -0300

    Lipar campos quando apagar todos os valores

--- lib/app/controllers/conrsion_controller.dart
+++ lib/app/controllers/conrsion_controller.dart
@@ -29,27 +29,50 @@ class ConversionCotroller{
     }
 
     void converterReal(String text){
+      if (text.isEmpty){
+        limparCampos();
+    }
      doge.text = ((double.parse(text))/moedas.doge).toStringAsFixed(2);
      xrp.text = ((double.parse(text))/moedas.xrp).toStringAsFixed(2);
      dol.text = ((double.parse(text))/moedas.dolar).toStringAsFixed(2);
+     
     }
     void converterDoge(String text){
+       if (text.isEmpty){
+        limparCampos();
+    }
      double convert_real = (moedas.doge*double.parse(text));
      real.text = (convert_real).toStringAsFixed(2);
      xrp.text = (convert_real/moedas.xrp).toStringAsFixed(2);
      dol.text = (convert_real/moedas.dolar).toStringAsFixed(2);
+    
     }
     void converterXrp(String text){
+       if (text.isEmpty){
+        limparCampos();
+    }
     double convert_real = (moedas.xrp*double.parse(text));
      doge.text = (convert_real/moedas.doge).toStringAsPrecision(2);
      real.text = (convert_real).toStringAsFixed(2);  
      dol.text = (convert_real/moedas.dolar).toStringAsFixed(2);
+    
     }
     void converterDol(String text){
+      if (text.isEmpty){
+        limparCampos();
+    }
     double convert_real = (moedas.dolar*double.parse(text));
      doge.text = (convert_real/moedas.doge).toStringAsPrecision(2);
      real.text = (convert_real).toStringAsFixed(2);  
      xrp.text = (convert_real/moedas.xrp).toStringAsFixed(2);
+     
+     }  
+
+    void limparCampos(){
+        real.text = "";
+        dol.text = "";
+        doge.text = "";
+        xrp.text = "";
     }
 
 }
